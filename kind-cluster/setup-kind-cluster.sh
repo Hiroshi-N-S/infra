95952e09b22c5eef71a4f47f84fc800f89e62d1a
Hiroshi.N.S <Hiroshi.N.September11@gmail.com>
2023-12-16 16:53:54 +0900

    fix: working directory

--- kind-cluster/setup-kind-cluster.sh
+++ kind-cluster/setup-kind-cluster.sh
@@ -5,40 +5,41 @@ WORK_DIR=$(pwd)
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 CONFIG_DIR=${SCRIPT_DIR}/config
 mkdir -p ${CONFIG_DIR}
-cd ${SCRIPT_DIR}
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # create cluster.
+cd ${SCRIPT_DIR}
 kind create cluster --config kind-cluster.yaml
-cd ${WORK_DIR}
 sleep 10 &
 wait
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # install external snapshotter
+cd ${SCRIPT_DIR}
 rm -rf external-snapshotter
 git clone https://github.com/kubernetes-csi/external-snapshotter.git external-snapshotter
 cd external-snapshotter
 kubectl kustomize client/config/crd | kubectl create -f -
 kubectl -n kube-system kustomize deploy/kubernetes/snapshot-controller | kubectl create -f -
-cd ${SCRIPT_DIR}
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # install synology csi driver. 
+cd ${SCRIPT_DIR}
 rm -rf synology-csi
 git clone https://github.com/SynologyOpenSource/synology-csi.git synology-csi
-sed -ie 's/ --short//' synology-csi/scripts/deploy.sh
+cd synology-csi
+sed -ie 's/ --short//' scripts/deploy.sh
 cp ${CONFIG_DIR}/synology/synology-client-info.yml \
-   synology-csi/config/client-info.yml
-rm synology-csi/deploy/kubernetes/v1.20/storage-class.yml
+   config/client-info.yml
+rm deploy/kubernetes/v1.20/storage-class.yml
 cp ${CONFIG_DIR}/synology/*.yaml \
-   synology-csi/deploy/kubernetes/v1.20/
-rm synology-csi/deploy/kubernetes/v1.20/namespace.yml
-# synology-csi/scripts/deploy.sh install --basic
-synology-csi/scripts/deploy.sh run
+   deploy/kubernetes/v1.20/
+rm deploy/kubernetes/v1.20/namespace.yml
+./scripts/deploy.sh install --all
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # apply ingress NGINX.
+cd ${SCRIPT_DIR}
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 sleep 10 &
 wait
@@ -49,6 +50,7 @@ kubectl wait --namespace ingress-nginx \
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # apply argocd.
+cd ${SCRIPT_DIR}
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install \
   --create-namespace --namespace argo-cd \
