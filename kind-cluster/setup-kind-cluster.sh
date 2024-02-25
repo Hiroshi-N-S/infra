eea773643b6e21bf1ebedfce39aaa2317516a463
Hiroshi.N.S <Hiroshi.N.September11@gmail.com>
2024-02-25 20:53:21 +0900

    refactoring

--- kind-cluster/setup-kind-cluster.sh
+++ kind-cluster/setup-kind-cluster.sh
@@ -13,29 +13,46 @@ kind create cluster --config kind-cluster.yaml
 sleep 10 &
 wait
 
+for node in $(kind get nodes -n kind-cluster); do
+  echo $node
+  docker exec "${node}" sed -ie 's/# node.startup = automatic/node.startup = automatic/' /etc/iscsi/iscsid.conf
+  docker exec "${node}" sed -ie 's/node.startup = manual/# node.startup = manual/' /etc/iscsi/iscsid.conf
+  docker exec "${node}" systemctl enable iscsid
+  docker exec "${node}" systemctl start iscsid
+done
+
+# # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+# # install external snapshotter
+# cd ${SCRIPT_DIR}
+# rm -rf external-snapshotter
+# git clone -b v6.3.2 https://github.com/kubernetes-csi/external-snapshotter.git external-snapshotter
+# cd external-snapshotter
+# kubectl kustomize client/config/crd | kubectl create -f -
+# kubectl -n kube-system kustomize deploy/kubernetes/snapshot-controller | kubectl create -f -
+
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-# install external snapshotter
-cd ${SCRIPT_DIR}
-rm -rf external-snapshotter
-git clone https://github.com/kubernetes-csi/external-snapshotter.git external-snapshotter
-cd external-snapshotter
-kubectl kustomize client/config/crd | kubectl create -f -
-kubectl -n kube-system kustomize deploy/kubernetes/snapshot-controller | kubectl create -f -
+# install nfs csi driver
+helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
+helm install \
+  csi-driver-nfs \
+  csi-driver-nfs/csi-driver-nfs \
+  --namespace kube-system \
+  --version v4.6.0
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # install synology csi driver. 
 cd ${SCRIPT_DIR}
 rm -rf synology-csi
-git clone https://github.com/SynologyOpenSource/synology-csi.git synology-csi
-cd synology-csi
-sed -ie 's/ --short//' scripts/deploy.sh
+git clone -b v1.1.3 https://github.com/SynologyOpenSource/synology-csi.git synology-csi
+sed -ie 's/ --short//' synology-csi/scripts/deploy.sh
 cp ${CONFIG_DIR}/synology/synology-client-info.yml \
-   config/client-info.yml
-rm deploy/kubernetes/v1.20/storage-class.yml
+   synology-csi/config/client-info.yml
+rm synology-csi/deploy/kubernetes/v1.20/storage-class.yml
 cp ${CONFIG_DIR}/synology/*.yaml \
-   deploy/kubernetes/v1.20/
-rm deploy/kubernetes/v1.20/namespace.yml
-./scripts/deploy.sh install --all
+   synology-csi/deploy/kubernetes/v1.20/
+rm synology-csi/deploy/kubernetes/v1.20/namespace.yml
+./synology-csi/scripts/deploy.sh install --basic
+#sh synology-csi/scripts/deploy.sh install --all
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # apply ingress NGINX.
@@ -56,7 +73,7 @@ helm install \
   --create-namespace --namespace argo-cd \
   argo-cd \
   argo/argo-cd \
-  --version 5.45.5 \
+  --version 6.2.5 \
   -f argo-cd.values.yaml
 sleep 10 &
 wait
@@ -64,4 +81,4 @@ kubectl wait --namespace argo-cd \
   --for=condition=ready \
   --all pods \
   --timeout=600s
-echo $(kubectl -n argo-cd get secret/argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) > ${CONFIG_DIR}/argo-cd-default-pw.txt
+# echo $(kubectl -n argo-cd get secret/argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) > ${CONFIG_DIR}/argo-cd-default-pw.txt
